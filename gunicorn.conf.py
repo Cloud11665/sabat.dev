2584927181f7b63a9055ad792b1fb10c498d2175
Bartek Sabat <Cloud11665@gmail.com>
2021-01-22 22:13:30 +0100

    fix worker type

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = "127.0.0.1:5000"
-workers = (multiprocessing.cpu_count() * 2 - 1 ) // 0.75
+workers = (multiprocessing.cpu_count() * 2 - 1 ) // 4 * 3
 worker_connections = 1000
 timeout = 30
 keepalive = 2
